a0439628a02329c64614d4de74e09cf1f4b70a74
EddPedd <130695504+EddPedd@users.noreply.github.com>
2023-07-14 17:09:15 +0200

    Fixat rörelser
    Sliders som bestämer antalet frames till stop och maxfart

--- ScaridyMouseProject/Assets/Scripts/PlayerMovement.cs
+++ ScaridyMouseProject/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //References to components
     public Rigidbody2D rb;
-    public float moveSpeed;
+
+    //Movement variables
+    public float maxMoveVelocity;
     public bool isMovingLeft = false;
     public bool isMovingRight = false;
-    
+    [Range(1, 50)]
+    [SerializeField]
+    private int framesToMax = 3;
+    private float velocityBeforeSpeed;
+    private float velocityPerFrame;
+
+    //Breaking variables
+    [Range (1, 50)]   [SerializeField]
+    private int framesToStop = 3;
+    public bool isBreaking = false;
+    private float velocityBeforeBreak;
+    private float breakPerFrame;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,45 +34,89 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Sätt kontrollera bools så att man bara kan gå ett ett håll samtidigt och 
-        if (Input.GetKey("a"))
+        //Decide values of variables related to moving left and start moving left. Actually moving left is under "void FixedUpdate"
+        if (Input.GetKeyDown("a") && !isMovingLeft)
         {
+            isBreaking = false; //Stop breaking and stop moving right
             isMovingRight = false;
+
+            velocityBeforeSpeed = rb.velocity.x; //
+            velocityPerFrame = (-maxMoveVelocity - velocityBeforeSpeed) / framesToMax; //adderar maxMoveVelocity i och med att hastigheten åt vänster är negativ, -- = +
+
             isMovingLeft = true;
+            Debug.Log("velocityBeforeSpeed = " + velocityBeforeSpeed + " and velocityPerFrame = " + velocityPerFrame);
+
         }
-        if (Input.GetKeyUp("a"))
+        if (Input.GetKeyUp("a")) //Stop moving left when releasing button
         {
             isMovingLeft = false;
         }
 
-        if (Input.GetKeyDown("d"))
+        //Decide values of variables related to moving right and start moving right. Actually moving right is under "void FixedUpdate"
+        if (Input.GetKeyDown("d") && !isMovingRight)
         {
+            isBreaking = false;
             isMovingLeft = false;
+
+            velocityBeforeSpeed = rb.velocity.x;
+            velocityPerFrame = (maxMoveVelocity - velocityBeforeSpeed) / framesToMax;
+
             isMovingRight = true;
+            Debug.Log("velocityBeforeSpeed = " +  velocityBeforeSpeed + " and velocityPerFrame = " + velocityPerFrame);
         }
-        if (Input.GetKeyUp("d"))
+        if (Input.GetKeyUp("d")) //Stop moving right when releasing button
         {
             isMovingRight = false;
         }
+
+        //Begin breaking and deciding values of related variables
+        if(!isMovingLeft && !isMovingRight && rb.velocity.x != 0 && !isBreaking)
+        {
+            isBreaking = true;
+            //Räkna ut hastigheten innan bromsandet börjat
+            velocityBeforeBreak = rb.velocity.x;
+            breakPerFrame = velocityBeforeBreak / framesToStop;
+            Debug.Log("breakPerFrame = " + breakPerFrame);
+        }
+
     }
 
     void FixedUpdate () 
     {
-        if (!isMovingLeft && !isMovingRight) 
+        //Beteende om man bromsar
+        if (isBreaking) 
         {
-            rb.velocity = new Vector2(0, 0);
-            return; 
+            float unclampedBreak = rb.velocity.x - breakPerFrame;
+
+            if(velocityBeforeBreak>=0)
+            {
+                rb.velocity = new Vector2(Mathf.Clamp(unclampedBreak, 0, velocityBeforeBreak), rb.velocity.y);
+            }
+            else if (velocityBeforeBreak<0)
+            {
+                rb.velocity = new Vector2(Mathf.Clamp(unclampedBreak, velocityBeforeBreak, 0), rb.velocity.y);
+            }
+                        
+            if(rb.velocity.x == 0)
+            {
+                isBreaking = !isBreaking;
+            }
         }
+
         //Beteende om man går vänster
         if (isMovingLeft)
         {
-            rb.velocity = new Vector2(-moveSpeed, 0);
+            float unclampedAcceleration = rb.velocity.x + velocityPerFrame; //Subtract to move left
+
+            rb.velocity = new Vector2(Mathf.Clamp(unclampedAcceleration, -maxMoveVelocity, velocityBeforeSpeed), rb.velocity.y);
         }
 
         //Beteende man går höger
         if (isMovingRight)
         {
-            rb.velocity = new Vector2(moveSpeed, 0);
+            float unclampedAcceleration = rb.velocity.x + velocityPerFrame; //Add to move right
+
+            rb.velocity = new Vector2(Mathf.Clamp(unclampedAcceleration, velocityBeforeSpeed,maxMoveVelocity ), rb.velocity.y);
         }
     }
 }
