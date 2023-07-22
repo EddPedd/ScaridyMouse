3bfbdfcfe95e94bccbb3361ca8be030a0c1bb407
EddPedd <130695504+EddPedd@users.noreply.github.com>
2023-07-22 20:00:53 +0200

    Same as just before something wrong with push

---
+++ ScaridyMouseProject/Assets/Scripts/HeartScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartScript : MonoBehaviour
+{
+    private GameObject playerObject;
+    private PlayerMovement player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindWithTag("Player");
+        player = playerObject.GetComponent<PlayerMovement>();
+        Debug.Log(playerObject.name);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Debug.Log(collider.gameObject.name + "picked up " + gameObject.name);
+            PickUp();
+        }
+    }
+
+    private void PickUp()
+    {
+        player.GainHealth(1);
+        GameObject.Destroy(gameObject);
+    }
+
+}
