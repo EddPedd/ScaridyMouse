c04fb3a5f32c678b2e4838a4f1315bc3190742c8
EddPedd <130695504+EddPedd@users.noreply.github.com>
2023-08-14 12:40:52 +0200

    Lagt till en övergång från en introScen till den riktiga scenen

--- ScaridyMouseProject/Assets/Scripts/Managers/GameManagerScript.cs
+++ ScaridyMouseProject/Assets/Scripts/Managers/GameManagerScript.cs
@@ -57,6 +57,11 @@ public class GameManagerScript : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    public void LoadNextScene(){
+        int currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneBuildIndex + 1);    
+        }
+
     public void QuitGame()
     {
         Application.Quit(); 
---
+++ ScaridyMouseProject/Assets/TransitionScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScript : MonoBehaviour
+{
+    //References
+    private GameObject gameManager;
+    private GameManagerScript manager;
+    
+    //Variables
+    private float startPosition;
+    private float middleOfScreen = 0f;
+    [SerializeField]
+    private float endPosition = -16f;
+    [SerializeField]
+    private AnimationCurve curve; 
+    [SerializeField]
+    private float transitionDuration;
+    private float percentageComplete;
+    private float transitionTime;
+    private float currentPosition;
+    
+
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("Manager");
+        manager = gameManager.GetComponent<GameManagerScript>();
+        
+        startPosition = transform.position.y;
+
+        //transform.position = new Vector3 (0f, startPosition, 0f);   //Make sure hat the image is centered on x and z axis
+
+        if(startPosition > 1  ){      
+            endPosition = middleOfScreen;   //Else it remains as the predetermined value of (0f, -16, 0f)
+        }  
+
+        Debug.Log(gameObject.name + "starts at the y position " + startPosition + " and is going to teh y position " + endPosition);             
+    }
+
+    void Update()
+    {
+        transitionTime += Time.deltaTime;
+        percentageComplete = transitionTime / transitionDuration;
+        float curveMultiplier = curve.Evaluate(percentageComplete);
+
+        if (percentageComplete >= 1){
+            if ( endPosition < -1){  //If the transitionImage has passed the screen and a New scene has been loaded
+                GameObject.Destroy(gameObject); //Destroy the transition Image and leave room for the rest of the scene
+                return;
+            }   
+            else{                   //Else (If the transitionImage is in the middle of the screen) load the next scene
+                manager.LoadNextScene();
+                return;
+            }               
+        }
+        else {
+            currentPosition = Mathf.Lerp(startPosition, endPosition, curveMultiplier);
+        }
+
+        transform.position = new Vector3(0f, currentPosition, 0f);        
+        
+        //transform.position.y = Vector3.Lerp(startPosition, endPosition, )
+
+    }
+}
