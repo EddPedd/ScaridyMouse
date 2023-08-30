f235cec3685d260ba01d9fd1da18a88a2cf312e7
EddPedd <130695504+EddPedd@users.noreply.github.com>
2023-08-30 16:11:46 +0200

    Further attempt att creating Obstacles2.0
    Added something to show dad later today to communicate what I want to achieve

--- ScaridyMouseProject/Assets/Scripts/Obstacle2.0/Obstacle2.cs
+++ ScaridyMouseProject/Assets/Scripts/Obstacle2.0/Obstacle2.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle2 : MonoBehaviour
+public class Obstacle2
 {
+    public int currentBounces;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,3 +18,40 @@ public class Obstacle2 : MonoBehaviour
         
     }
 }
+
+public class ObstacleColor{
+    public int maxBounces;
+    public Color color;
+    
+}
+
+public class ObstacleSieze{
+    public float scale;
+    public float linearDrag;
+    public float squeezeIndx;
+
+}
+
+public class ObstacleShape{
+    public Sprite spriteShape;
+
+    public virtual void OnCollisionEnter2D(Collider2D collider){
+        //To override for each instance of shape
+    }
+}
+
+public class Program{
+    public ObstacleShape Square; 
+    public ObstacleShape triangle; 
+    public ObstacleShape circle; 
+
+    public ObstacleColor red;
+    public ObstacleColor blue;
+    public ObstacleColor green;
+
+    public ObstacleSieze small;
+    public ObstacleSieze medium;
+    public ObstacleSieze large;
+}
+
+
